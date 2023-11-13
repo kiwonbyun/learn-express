04a05b4a4b7c6cd7a0a025e8eac606b6dd405afc
kiwonbyun <bkw9603@naver.com>
2023-11-13 22:42:31 +0900

    connect sequelize

--- app.js
+++ app.js
@@ -7,13 +7,28 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const fs = require('fs');
+const nunjucks = require('nunjucks');
+const { sequelize } = require('./models');
 
 dotenv.config();
 const indexRouter = require('./routes');
-const userRouter = require('./routes/user');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
 const app = express();
 app.set('port', process.env.PORT || 3000);
-
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -50,15 +65,20 @@ const upload = multer({
 });
 
 app.use('/', indexRouter);
-app.use('/user', userRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send('Not Found');
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
